21276d979fe6e5911f5bb7968ea4f2f84091bc45
Efran Nathanael <37508513+BadAccuracyID@users.noreply.github.com>
2023-09-05 12:55:24 +0700

    Added file selector with preview

--- src/components/home/CreatePost.tsx
+++ src/components/home/CreatePost.tsx
@@ -1,9 +1,41 @@
 import {User} from "../../lib/gql/graphql.ts";
 import "../../styles/create-post.scss";
 import {AiFillCloseCircle} from "react-icons/ai";
+import {BiSolidUserCircle} from "react-icons/bi";
+import React, {createRef, useState} from "react";
+import {BsCameraVideoFill} from "react-icons/bs";
+import {HiPhoto} from "react-icons/hi2";
 
 export default function CreatePostComponent({user, onClose}: { user: User, onClose: () => void }) {
-    const profilePicture = "https://student-activity.binus.ac.id/himmat/wp-content/uploads/sites/14/2023/03/jere-pp.jpg";
+    const profilePicture = user.profilePicture;
+
+    const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
+    const fileInputRef: React.Ref<HTMLInputElement> = createRef();
+
+    const onFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files) {
+            const files = Array.from(event.target.files);
+            if (files.length > 10) {
+                return;
+            }
+
+            setSelectedFiles(files);
+        }
+    };
+
+    const onFileRemove = (index: number): React.MouseEventHandler<HTMLButtonElement> => {
+        return () => {
+            setSelectedFiles(prev => prev.filter((_, i) => i !== index));
+        }
+    }
+
+    const onButtonClick = () => {
+        if (!fileInputRef.current) {
+            return;
+        }
+
+        fileInputRef.current.click();
+    };
 
     return (
         <div className="create-post-popup">
@@ -20,7 +52,8 @@ export default function CreatePostComponent({user, onClose}: { user: User, onClo
                 <br/>
 
                 <div className="create-post-popup-container-user">
-                    <img className="create-post-popup-container-user-profile-picture" src={profilePicture}/>
+                    {profilePicture ? <img className="post-header-profile-picture" src={profilePicture}/> :
+                        <BiSolidUserCircle className="post-header-profile-picture-null"/>}
                     <div className="create-post-popup-container-user-sub-info">
                         <p className="create-post-popup-container-user-sub-info-name">
                             {user.firstName} {user.lastName}
@@ -40,9 +73,42 @@ export default function CreatePostComponent({user, onClose}: { user: User, onClo
                         placeholder={`What's on your mind, ${user.firstName}?`}/>
                 </div>
 
-                {/*<div className="create-post-popup-container-footer">*/}
-                {/*    <input type="file"/>*/}
-                {/*</div>*/}
+                <div className="create-post-popup-container-footer">
+                    <input
+                        type="file"
+                        onChange={onFileChange}
+                        multiple
+                        ref={fileInputRef}
+                    />
+
+                    <div className="create-post-popup-container-footer-container"
+                         onClick={onButtonClick}>
+                        <div className="create-post-popup-container-footer-container-button">
+                            Upload File (Max. 10 files)
+                        </div>
+
+                        <div className="create-post-popup-container-footer-container-icons-container">
+                            <BsCameraVideoFill
+                                className="create-post-popup-container-footer-container-icons-icon-camera"/>
+                            <HiPhoto
+                                className="create-post-popup-container-footer-container-icons-icon-photo"/>
+                        </div>
+                    </div>
+
+                    <div className="create-post-popup-container-footer-preview">
+                        {selectedFiles.map((file, index) => {
+                            const fileURL = URL.createObjectURL(file);
+                            return (
+                                <div className="photo-container" key={index}>
+                                    <button className="delete-button" onClick={onFileRemove(index)}>
+                                        <AiFillCloseCircle/>
+                                    </button>
+                                    <img src={fileURL} alt="preview"/>
+                                </div>
+                            );
+                        })}
+                    </div>
+                </div>
 
                 <div className="create-post-popup-container-button-unavailable">
                     Post
--- src/lib/controllers/post-controller.ts
+++ src/lib/controllers/post-controller.ts
@@ -1,5 +1,5 @@
 import {graphql} from "../gql";
-import {Post} from "../gql/graphql.ts";
+import {Post, PostInput} from "../gql/graphql.ts";
 import {client} from "../../main.tsx";
 import * as assert from "assert";
 
@@ -137,6 +137,131 @@ const GET_POSTS_QUERY = graphql(`
     }
 `);
 
+// createPost(input: PostInput!): Post
+// input PostInput {
+//     title: String!
+//     audience: Audience!
+//
+//     textContent: String!
+//     imageContent: [String!]
+//     videoContent: [String!]
+//
+//     taggedUsers: [ID!]
+//     mentionedUsers: [ID!]
+//     hashtags: [String!]
+// }
+
+const CREATE_POST_QUERY = graphql(`
+    mutation createPost($input: PostInput!) {
+        createPost(input: $input) {
+            id
+            title
+            audience
+            author {
+                id
+                firstName
+                lastName
+                activated
+                username
+                email
+                dateOfBirth
+                gender
+            }
+
+            sharedBy {
+                id
+                firstName
+                lastName
+                activated
+                username
+                email
+                dateOfBirth
+                gender
+            }
+            likedBy {
+                id
+                firstName
+                lastName
+                activated
+                username
+                email
+                dateOfBirth
+                gender
+            }
+            comments {
+                id
+                postId
+                author {
+                    id
+                    firstName
+                    lastName
+                    activated
+                    username
+                    email
+                    dateOfBirth
+                    gender
+                }
+                textContent
+                replies {
+                    id
+                    postId
+                    author {
+                        id
+                        firstName
+                        lastName
+                        activated
+                        username
+                        email
+                        dateOfBirth
+                        gender
+                    }
+                    textContent
+                    likedBy {
+                        id
+                        firstName
+                        lastName
+                        activated
+                        username
+                        email
+                        dateOfBirth
+                        gender
+                    }
+                    createdAt
+                }
+                likedBy {
+                    id
+                    firstName
+                    lastName
+                    activated
+                    username
+                    email
+                    dateOfBirth
+                    gender
+                }
+                createdAt
+            }
+
+            textContent
+            imageContent
+            videoContent
+
+            taggedUsers {
+                id
+                firstName
+                lastName
+                activated
+                username
+                email
+                dateOfBirth
+                gender
+            }
+            hashtags
+
+            createdAt
+        }
+    }
+`);
+
 export async function getPosts(pageNumber: number, limit: number): Promise<ControllerResponse<Post[]>> {
     try {
         const {data, errors} = await client.query({
@@ -204,6 +329,80 @@ export async function getPosts(pageNumber: number, limit: number): Promise<Contr
         let errorMsg = 'Error executing getPosts';
         if (error instanceof Error) {
             console.error('Error executing getPosts:', error);
+            errorMsg = error.message;
+        }
+
+        return {
+            success: false,
+            errorMsg: [errorMsg],
+            data: null,
+        };
+    }
+}
+
+export async function createPosts(input: PostInput): Promise<ControllerResponse<Post>> {
+    try {
+        const {data, errors} = await client.mutate({
+            mutation: CREATE_POST_QUERY,
+            variables: {
+                input,
+            },
+        });
+
+        if (errors) {
+            return {
+                success: false,
+                errorMsg: errors.map(e => e.message),
+                data: null,
+            }
+        }
+
+        if (!data?.createPost) {
+            return {
+                success: false,
+                errorMsg: ['Invalid response from server'],
+                data: null,
+            }
+        }
+
+        const it = data.createPost;
+        assert(it !== null);
+
+        const post: Post = {
+            id: it.id,
+            title: it.title,
+            audience: it.audience,
+            author: {
+                id: it.author.id,
+                email: it.author.email,
+                username: it.author.username,
+                dateOfBirth: it.author.dateOfBirth,
+                firstName: it.author.firstName,
+                lastName: it.author.lastName,
+                gender: it.author.gender,
+                activated: it.author.activated,
+            },
+            comments: it.comments,
+            createdAt: it.createdAt,
+            hashtags: it.hashtags,
+            imageContent: it.imageContent,
+            likedBy: it.likedBy,
+            sharedBy: it.sharedBy,
+            taggedUsers: it.taggedUsers,
+            textContent: it.textContent,
+            videoContent: it.videoContent,
+        }
+
+        return {
+            success: true,
+            errorMsg: null,
+            data: post,
+        }
+
+    } catch (error) {
+        let errorMsg = 'Error executing createPosts';
+        if (error instanceof Error) {
+            console.error('Error executing createPosts:', error);
             errorMsg = error.message;
         }
 

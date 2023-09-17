0bcff697b7dd9042cf6fe52464d718d5f80126a0
Efran Nathanael <37508513+BadAccuracyID@users.noreply.github.com>
2023-09-17 21:14:28 +0700

    Improved messanger page UI

--- src/components/messenger/Conversation.tsx
+++ src/components/messenger/Conversation.tsx
@@ -1,5 +1,6 @@
 import {Conversation} from "../../lib/gql/graphql.ts";
 import "../../styles/messenger.scss";
+import {BsChatDots} from "react-icons/bs";
 
 export default function Conversations({conversations, onSelectConversation}: {
     conversations: Conversation[],
@@ -26,19 +27,19 @@ function ConversationCard({conversation, onSelect}: {
         <div className="conversation-card" onClick={() => {
             onSelect(conversation);
         }}>
-            <img className="conversation-card-picture"
-                 src={conversation.members[0].profilePicture!}
-                 alt="conversation picture"/>
+            <BsChatDots className="conversation-card-picture-null"/>
 
             <div className="conversation-right">
                 <div className="conversation-card-right-title">
                     {conversation.title}
                 </div>
 
-                <div className="conversation-card-right-last-message">
-                    {conversation.messages[conversation.messages.length - 1].sender.username}: &nbsp;
-                    {conversation.messages[conversation.messages.length - 1].content}
-                </div>
+                {conversation.messages.length > 0 ?
+                    <div className="conversation-card-right-last-message">
+                        {conversation.messages[conversation.messages.length - 1].sender.username}: &nbsp;
+                        {conversation.messages[conversation.messages.length - 1].content}
+                    </div> : <></>
+                }
             </div>
         </div>
     )

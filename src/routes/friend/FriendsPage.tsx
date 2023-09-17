b09dfe5064a593568cc1275516d4265dbdc340d5
Efran Nathanael <37508513+BadAccuracyID@users.noreply.github.com>
2023-09-17 20:25:40 +0700

    Actual friends page

--- src/routes/friend/FriendsPage.tsx
+++ src/routes/friend/FriendsPage.tsx
@@ -2,13 +2,29 @@ import {useLoaderData} from "react-router-dom";
 import {User} from "../../lib/gql/graphql.ts";
 import NavigationBar from "../../components/NavigationBar.tsx";
 import {
-    AllFriendsComponent, FavoriteFriendsComponent,
+    AllFriendsComponent,
+    FavoriteFriendsComponent,
     FriendRecommendationComponent,
     FriendRequestsComponent
 } from "../../components/friends/FriendComponent.tsx";
+import {useState} from "react";
+import {AiFillHome, AiFillStar, AiOutlineUser, AiOutlineUserAdd, AiOutlineUsergroupAdd} from "react-icons/ai";
+
+enum MenuPage {
+    HOME,
+    PENDING,
+    RECOMENDATION,
+    FRIENDS,
+    FAVORITES,
+}
 
 export default function FriendsPage() {
     const currentUser = useLoaderData() as User;
+    const [page, setPage] = useState<MenuPage>(MenuPage.HOME);
+
+    function changePage(page: MenuPage) {
+        setPage(page);
+    }
 
     return (
         <div>
@@ -23,16 +39,68 @@ export default function FriendsPage() {
                         </div>
                         <br/>
                     </div>
+
+                    <div className="friends-left-buttons">
+                        <div className="friends-left-buttons-container"
+                             onClick={() => changePage(MenuPage.HOME)}>
+                            <AiFillHome
+                                className={"friends-left-buttons-icon" + (page === MenuPage.HOME ? "-active" : "")}/>
+                            <div>Home</div>
+                        </div>
+                        <div className="friends-left-buttons-container"
+                             onClick={() => changePage(MenuPage.PENDING)}>
+                            <AiOutlineUserAdd
+                                className={"friends-left-buttons-icon" + (page === MenuPage.PENDING ? "-active" : "")}/>
+                            <div>Pending</div>
+                        </div>
+                        <div className="friends-left-buttons-container"
+                             onClick={() => changePage(MenuPage.RECOMENDATION)}>
+                            <AiOutlineUsergroupAdd
+                                className={"friends-left-buttons-icon" + (page === MenuPage.RECOMENDATION ? "-active" : "")}/>
+                            <div>Recommendation</div>
+                        </div>
+                        <div className="friends-left-buttons-container"
+                             onClick={() => changePage(MenuPage.FRIENDS)}>
+                            <AiOutlineUser
+                                className={"friends-left-buttons-icon" + (page === MenuPage.FRIENDS ? "-active" : "")}/>
+                            <div>Friends</div>
+                        </div>
+                        <div className="friends-left-buttons-container"
+                             onClick={() => changePage(MenuPage.FAVORITES)}>
+                            <AiFillStar
+                                className={"friends-left-buttons-icon" + (page === MenuPage.FAVORITES ? "-active" : "")}/>
+                            <div>Favorites</div>
+                        </div>
+                    </div>
                 </div>
 
 
                 <div className="friends-content">
-                    <FriendRecommendationComponent currentUser={currentUser}/>
-                    <FriendRequestsComponent currentUser={currentUser}/>
-                    <AllFriendsComponent user={currentUser} currentUser={currentUser}/>
-                    <FavoriteFriendsComponent user={currentUser} currentUser={currentUser}/>
+                    <PageContent currentUser={currentUser} page={page}/>
                 </div>
             </div>
         </div>
     )
+}
+
+function PageContent({currentUser, page}: { currentUser: User, page: MenuPage }) {
+    switch (page) {
+        case MenuPage.HOME:
+            return (
+                <div>
+                    <FriendRequestsComponent currentUser={currentUser}/>
+                    <FriendRecommendationComponent currentUser={currentUser}/>
+                    <AllFriendsComponent user={currentUser} currentUser={currentUser}/>
+                    <FavoriteFriendsComponent user={currentUser} currentUser={currentUser}/>
+                </div>
+            )
+        case MenuPage.PENDING:
+            return (<FriendRequestsComponent currentUser={currentUser}/>)
+        case MenuPage.RECOMENDATION:
+            return (<FriendRecommendationComponent currentUser={currentUser}/>)
+        case MenuPage.FRIENDS:
+            return (<AllFriendsComponent user={currentUser} currentUser={currentUser}/>)
+        case MenuPage.FAVORITES:
+            return (<FavoriteFriendsComponent user={currentUser} currentUser={currentUser}/>)
+    }
 }

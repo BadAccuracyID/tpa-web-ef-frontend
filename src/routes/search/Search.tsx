ad47aad466949bffcb8db52cd8df72038fbd3dad
Efran Nathanael <37508513+BadAccuracyID@users.noreply.github.com>
2023-09-17 20:32:32 +0700

    Search page filtering

--- src/routes/search/Search.tsx
+++ src/routes/search/Search.tsx
@@ -2,15 +2,28 @@ import {Await, useLoaderData, useNavigate, useSearchParams} from "react-router-d
 import NavigationBar from "../../components/NavigationBar.tsx";
 import {Group, Post, SearchResult, User} from "../../lib/gql/graphql.ts";
 import "../../styles/search.scss";
-import {Suspense} from "react";
+import {Suspense, useState} from "react";
 import {searchQuery} from "../../lib/controllers/search-controller.ts";
 import {BiSolidUserCircle} from "react-icons/bi";
 import {FullPageLoading} from "../../components/loading/LoadingComponents.tsx";
+import {AiFillHome, AiOutlineUser, AiOutlineUserAdd, AiOutlineUsergroupAdd} from "react-icons/ai";
+
+enum MenuPage {
+    ALL,
+    PROFILE,
+    POST,
+    GROUP
+}
 
 export default function SearchPage() {
     const navigate = useNavigate();
     const currentUser = useLoaderData() as User;
     const [searchParams] = useSearchParams();
+    const [page, setPage] = useState<MenuPage>(MenuPage.ALL);
+
+    function changePage(page: MenuPage) {
+        setPage(page);
+    }
 
     async function search(): Promise<SearchResult[] | null> {
         const query = searchParams.get("q");
@@ -27,9 +40,7 @@ export default function SearchPage() {
     }
 
     function redirectProfile(user: User) {
-        //'/profile/' + user.id
         navigate('/profile/' + user.id);
-
     }
 
     return (
@@ -44,6 +55,33 @@ export default function SearchPage() {
                         </div>
                         <br/>
                     </div>
+
+                    <div className="search-left-buttons">
+                        <div className="search-left-buttons-container"
+                             onClick={() => changePage(MenuPage.ALL)}>
+                            <AiFillHome
+                                className={"search-left-buttons-icon" + (page === MenuPage.ALL ? "-active" : "")}/>
+                            <div>All</div>
+                        </div>
+                        <div className="search-left-buttons-container"
+                             onClick={() => changePage(MenuPage.PROFILE)}>
+                            <AiOutlineUserAdd
+                                className={"search-left-buttons-icon" + (page === MenuPage.PROFILE ? "-active" : "")}/>
+                            <div>Profiles</div>
+                        </div>
+                        <div className="search-left-buttons-container"
+                             onClick={() => changePage(MenuPage.POST)}>
+                            <AiOutlineUsergroupAdd
+                                className={"search-left-buttons-icon" + (page === MenuPage.POST ? "-active" : "")}/>
+                            <div>Posts</div>
+                        </div>
+                        <div className="search-left-buttons-container"
+                             onClick={() => changePage(MenuPage.GROUP)}>
+                            <AiOutlineUser
+                                className={"search-left-buttons-icon" + (page === MenuPage.GROUP ? "-active" : "")}/>
+                            <div>Groups</div>
+                        </div>
+                    </div>
                 </div>
 
                 <div className="search-content">
@@ -54,54 +92,70 @@ export default function SearchPage() {
                                     return <h1>Nothing found</h1>
                                 }
 
-                                return result.map((item: SearchResult) => {
-                                    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                                    // @ts-ignore
-                                    const userType: string = item['__typename'];
-                                    if (userType === "User") {
-                                        const user = item as User;
-                                        return (
-                                            <div className="search-content search-content-user" onClick={() => {
-                                                redirectProfile(user)
-                                            }}>
-                                                {user.profilePicture ?
-                                                    <img
-                                                        src={user.profilePicture!}
-                                                        className="search-content-user-picture"
-                                                        alt="Profile picture"
-                                                    />
-                                                    :
-                                                    <BiSolidUserCircle className="search-content-user-null-picture"/>
-                                                }
-                                                <div className="search-content-user-info">
-                                                    <div className="search-content-user-info-name">
-                                                        {user.firstName} {user.lastName}
-                                                    </div>
+                                return result
+                                    .filter((item: SearchResult) => {
+                                        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                                        // @ts-ignore
+                                        const userType: string = item['__typename'];
+                                        if (userType === "User") {
+                                            return page === MenuPage.ALL || page === MenuPage.PROFILE;
+                                        } else if (userType === "Post") {
+                                            return page === MenuPage.ALL || page === MenuPage.POST;
+                                        } else if (userType === "Group") {
+                                            return page === MenuPage.ALL || page === MenuPage.GROUP;
+                                        } else {
+                                            return false;
+                                        }
+                                    })
+                                    .map((item: SearchResult) => {
+                                        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                                        // @ts-ignore
+                                        const userType: string = item['__typename'];
+                                        if (userType === "User") {
+                                            const user = item as User;
+                                            return (
+                                                <div className="search-content search-content-user" onClick={() => {
+                                                    redirectProfile(user)
+                                                }}>
+                                                    {user.profilePicture ?
+                                                        <img
+                                                            src={user.profilePicture!}
+                                                            className="search-content-user-picture"
+                                                            alt="Profile picture"
+                                                        />
+                                                        :
+                                                        <BiSolidUserCircle
+                                                            className="search-content-user-null-picture"/>
+                                                    }
+                                                    <div className="search-content-user-info">
+                                                        <div className="search-content-user-info-name">
+                                                            {user.firstName} {user.lastName}
+                                                        </div>
 
-                                                    <div className="search-content-user-info-username">
-                                                        {user.username}
+                                                        <div className="search-content-user-info-username">
+                                                            {user.username}
+                                                        </div>
                                                     </div>
                                                 </div>
-                                            </div>
-                                        )
-                                    } else if (userType === "Post") {
-                                        const post = item as Post;
-                                        return (
-                                            <div>
-                                                <h1>{post.title}</h1>
-                                            </div>
-                                        )
-                                    } else if (userType === "Group") {
-                                        const group = item as Group;
-                                        return (
-                                            <div>
-                                                <h1>{group.name}</h1>
-                                            </div>
-                                        )
-                                    } else {
-                                        return <div>Null</div>
-                                    }
-                                });
+                                            )
+                                        } else if (userType === "Post") {
+                                            const post = item as Post;
+                                            return (
+                                                <div>
+                                                    <h1>{post.title}</h1>
+                                                </div>
+                                            )
+                                        } else if (userType === "Group") {
+                                            const group = item as Group;
+                                            return (
+                                                <div>
+                                                    <h1>{group.name}</h1>
+                                                </div>
+                                            )
+                                        } else {
+                                            return <div>Null</div>
+                                        }
+                                    });
                             }}
                         </Await>
                     </Suspense>

3b26003d93d103d918b5852a078e94a76cf6b390
Efran Nathanael <37508513+BadAccuracyID@users.noreply.github.com>
2023-09-18 14:39:12 +0700

    Better comment component

--- src/components/post/CommentComponent.tsx
+++ src/components/post/CommentComponent.tsx
@@ -1,10 +1,32 @@
 import {Comment} from "../../lib/gql/graphql.ts";
 import "../../styles/post.scss";
+import {BiSolidUserCircle} from "react-icons/bi";
 
 export default function CommentComponent({comment}: { comment: Comment }) {
+    const author = comment.author;
+    const profilePicture = author.profilePicture;
     return (
         <div className="comment">
-            {comment.textContent}
+            {profilePicture ? <img className="comment-picture" src={profilePicture}/> :
+                <BiSolidUserCircle className="comment-picture-null"/>}
+
+            <div className="comment-right">
+                <div className="comment-right-name">
+                    {author.firstName} {author.lastName}
+                </div>
+                <div className="comment-right-content">
+                    {comment.textContent}
+                </div>
+
+                <div className="comment-right-buttons">
+                    <div className="comment-right-buttons-like">
+                        Like
+                    </div>
+                    <div className="comment-right-buttons-comment">
+                        Comment
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }

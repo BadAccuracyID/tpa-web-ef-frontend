e4b4b4fa81c1b62818260e2658c6ea33fbfb10d9
Efran Nathanael <37508513+BadAccuracyID@users.noreply.github.com>
2023-09-19 16:59:03 +0700

    Password resetting

--- src/routes/auth/PasswordReset.tsx
+++ src/routes/auth/PasswordReset.tsx
@@ -1,12 +1,12 @@
-import {Await, Link, redirect, useParams} from "react-router-dom";
+import {Link, redirect, useNavigate, useParams} from "react-router-dom";
 import {onSetPassword, onVerifyResetPasswordToken} from "../../lib/controllers/auth-controller.ts";
-import {FullPageCircularLoading} from "../../components/loading/LoadingComponents.tsx";
-import {Suspense, useState} from "react";
+import {useEffect, useState} from "react";
 import {User} from "../../lib/gql/graphql.ts";
 import FooterComponent from "../../components/Footer.tsx";
 import {toast} from "react-toastify";
 
 export default function PasswordResetPage() {
+    const navigate = useNavigate();
     const {token} = useParams();
 
     const [newPassword, setNewPassword] = useState('');
@@ -24,8 +24,20 @@ export default function PasswordResetPage() {
         }
 
         const encodedToken = encodeURIComponent(token);
+        console.log(encodedToken)
         return await onVerifyResetPasswordToken(encodedToken);
     }
+
+    useEffect(() => {
+        validateToken().then((response) => {
+            if (!response.success) {
+                redirect('/auth/login');
+                return;
+            }
+
+            setUser(response.data);
+        });
+    }, []);
 
     async function onSubmitReset() {
         if (!user) {
@@ -69,73 +81,65 @@ export default function PasswordResetPage() {
             closeOnClick: true,
             draggable: true,
         });
-        redirect('/auth/login');
+    }
+
+    if (!user) {
+        return (
+            <div>
+                <div className="account-recovery">
+                    <div className="container-recovery">
+                        <h2>Invalid Token</h2>
+                        <hr className="divider"/>
+                        <p>
+                            The token you have provided is invalid. Please try again.
+                        </p>
+
+                        <hr className="divider"/>
+
+                        <Link to={'/auth/login'} className="cancel-button">Back</Link>
+                    </div>
+                </div>
+                <FooterComponent/>
+            </div>
+        )
     }
 
     return (
-        <Suspense fallback={<FullPageCircularLoading/>}>
-            <Await resolve={validateToken()}>
-                {(response: ControllerResponse<User>) => {
-                    if (!response.success) {
-                        return (
-                            <div>
-                                <div className="account-recovery">
-                                    <div className="container-recovery">
-                                        <h2>Invalid Token</h2>
-                                        <hr className="divider"/>
-                                        <p>
-                                            The token you have provided is invalid. Please try again.
-                                        </p>
+        <div>
+            <div className="account-recovery">
+                <div className="container-recovery">
+                    <h2>Reset Password</h2>
+                    <hr className="divider"/>
+                    <p>
+                        Please enter your new password.
+                    </p>
+                    <input
+                        type="password"
+                        placeholder="New Password"
+                        value={newPassword}
+                        onChange={(e) => setNewPassword(e.target.value)}
+                    />
+                    <input
+                        type="password"
+                        placeholder="Confirm Password"
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                    />
 
-                                        <hr className="divider"/>
+                    <hr className="divider"/>
 
-                                        <Link to={'/auth/login'} className="cancel-button">Back</Link>
-                                    </div>
-                                </div>
-                                <FooterComponent/>
-                            </div>
-                        )
-                    }
-
-                    setUser(response.data!);
-                    return (
-                        <div>
-                            <div className="account-recovery">
-                                <div className="container-recovery">
-                                    <h2>Reset Password</h2>
-                                    <hr className="divider"/>
-                                    <p>
-                                        Please enter your new password.
-                                    </p>
-                                    <input
-                                        type="password"
-                                        placeholder="New Password"
-                                        value={newPassword}
-                                        onChange={(e) => setNewPassword(e.target.value)}
-                                    />
-                                    <input
-                                        type="password"
-                                        placeholder="Confirm Password"
-                                        value={confirmPassword}
-                                        onChange={(e) => setConfirmPassword(e.target.value)}
-                                    />
-
-                                    <hr className="divider"/>
-
-                                    <Link to={'/auth/login'} className="cancel-button">Cancel</Link>
-                                    <button
-                                        className="search-button"
-                                        onClick={() => onSubmitReset()}
-                                    >
-                                        Reset
-                                    </button>
-                                </div>
-                            </div>
-                            <FooterComponent/>
-                        </div>
-                    )
-                }}
-            </Await>
-        </Suspense>
+                    <div className="buttons">
+                        <Link to={'/auth/login'} className="cancel-button">Cancel</Link>
+                        <button
+                            className="search-button"
+                            onClick={() => onSubmitReset().then(() => navigate('/auth/login'))}
+                        >
+                            Reset
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <FooterComponent/>
+        </div>
     )
 }
